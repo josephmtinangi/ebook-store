7404d42bf24a44c39395166888fee859efca64b3
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-11-12 18:23:42 +0000

    Create categories table seeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CategoriesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
+        DB::table('categories')->delete();
+
+        factory('App\Models\Category', 10)->create();
     }
 }
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -7,8 +7,6 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">{{ $book->title }}</div>
-
                 <div class="panel-body">
                     <div class="media">
                         <a class="pull-left" href="{{ route('books.show', $book->slug) }}">
