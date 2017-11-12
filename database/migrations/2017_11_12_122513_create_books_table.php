430dcb7c47a505d585d7cab8ff6cbe4a913b6292
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-11-12 12:32:04 +0000

    Create Category and Book models and migrations

---
+++ database/migrations/2017_11_12_122513_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('image_url')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+            
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
